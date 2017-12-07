2dabe3790a6e242b651163789c3923748bc955e3
graymur <graymur@mail.ru>
2017-12-07 19:20:43 +0300

    Prevent fetching indicator's date for empty year

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 2. Find out why `@@router/LOCATION_CHANGE` event is fired on server-rendered pages.
 3. Indicator page - sort countries by value and name
 4. Indicator page - clear up
+5. Indicator page - load one indicator's year, open another indicater - empty year will load
--- src/shared/logic/loadIfNeeded/indicatorData.js
+++ src/shared/logic/loadIfNeeded/indicatorData.js
@@ -1,8 +1,7 @@
 export default (props, dispatchLoadingAction) => {
 	const {match, currentYear} = props;
 
-	if (match.params.year !== currentYear) {
-		console.log('CURRENT YEAR', currentYear);
+	if (match.params.year && match.params.year !== currentYear) {
 		return dispatchLoadingAction(match.params.indicatorId, match.params.year);
 	}
 };
