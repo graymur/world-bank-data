53190cbb9d3eaed32c686802f061ad337449e7bf
graymur <graymur@mail.ru>
2017-12-13 21:01:52 +0300

    Added API to prod server

--- src/server/serverProd.js
+++ src/server/serverProd.js
@@ -3,12 +3,14 @@ import express from 'express';
 import opener from 'opener';
 import fs from 'fs';
 import path from 'path';
-import './ssr/stubAssetsRequires';
 import compression from 'compression';
+import './ssr/stubAssetsRequires';
+import api from './api/1/index';
 
 const app = express();
 app.use(compression());
 app.use(express.static(config.buildDir));
+app.use('/api/1/', api);
 
 const htmlTemplate = fs.readFileSync(path.join(config.buildDir, 'index.template.html')).toString();
 
