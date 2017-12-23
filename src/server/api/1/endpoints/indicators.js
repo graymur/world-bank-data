db1b593b854b35f95257d3097ac52465d406b140
graymur <graymur@mail.ru>
2017-12-23 20:11:29 +0100

    Fixed linting error

--- src/server/api/1/endpoints/indicators.js
+++ src/server/api/1/endpoints/indicators.js
@@ -1,4 +1,3 @@
-import dataSource from 'server/dataSource';
 import wrapWithErrorHandling from '../util/wrapWithErrorHandling';
 
 export default dataSource => wrapWithErrorHandling(async (req, res) => {
