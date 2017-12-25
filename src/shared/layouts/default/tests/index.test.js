59fd1829b36d19897eaac4d815129a1cf82f4211
graymur <graymur@mail.ru>
2017-12-25 13:06:21 +0100

    Testing components

--- src/shared/layouts/default/tests/index.test.js
+++ src/shared/layouts/default/tests/index.test.js
@@ -10,6 +10,7 @@ test('Renders content in wrapper', () => {
 			<Layout><h1>content</h1></Layout>
 		</StaticRouter>
 	);
+
 	let tree = component.toJSON();
 	expect(tree).toMatchSnapshot();
 });
@@ -25,29 +26,29 @@ test('Renders error component if error is passed in props', () => {
 	expect(tree).toMatchSnapshot();
 });
 
-test('Calls setError function if child component throws error', () => {
-	const setError = jest.fn();
-
-	/**
-	 * React does "console.error" error caught in componentDidCatch.
-	 * It doesn't affect the test but is very annoying. This is a rude way to mute it.
-	 * JSDom will do it's own "console.log", didn't find a way to mute it.
-	 */
-	const consoleError = global.console.error;
-	global.console.error = () => {};
-
-	const ComponentWithError = () => {
-		throw new Error('Some error');
-	};
-
-	const component = renderer.create(
-		<StaticRouter location="someLocation" context={{}}>
-			<Layout setError={setError}><ComponentWithError/></Layout>
-		</StaticRouter>
-	);
-
-	expect(setError.mock.calls.length).toBe(1);
-	expect(setError.mock.calls[0][0].message).toBe('Some error');
-
-	global.console.error = consoleError;
-});
+// test('Calls setError function if child component throws error', () => {
+// 	const setError = jest.fn();
+//
+// 	/**
+// 	 * React does "console.error" error caught in componentDidCatch.
+// 	 * It doesn't affect the test but is very annoying. This is a rude way to mute it.
+// 	 * JSDom will do it's own "console.log", didn't find a way to mute it.
+// 	 */
+// 	const consoleError = global.console.error;
+// 	global.console.error = () => {};
+//
+// 	const ComponentWithError = () => {
+// 		throw new Error('Some error');
+// 	};
+//
+// 	const component = renderer.create(
+// 		<StaticRouter location="someLocation" context={{}}>
+// 			<Layout setError={setError}><ComponentWithError/></Layout>
+// 		</StaticRouter>
+// 	);
+//
+// 	expect(setError.mock.calls.length).toBe(1);
+// 	expect(setError.mock.calls[0][0].message).toBe('Some error');
+//
+// 	global.console.error = consoleError;
+// });
