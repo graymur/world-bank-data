02dfbfd4ab8337680bb14b9df70fb94f7dfc909f
graymur <graymur@mail.ru>
2018-01-31 10:32:16 +0100

    Dependencies versions bump

--- src/shared/redux/reducer.js
+++ src/shared/redux/reducer.js
@@ -2,7 +2,6 @@ import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
 
 import shared, {SHARED_REDUCER_KEY} from 'shared/logic/shared/reducer';
-import country, {COUNTRY_REDUCER_KEY} from 'shared/logic/country/reducer';
 import indicators, {INDICATORS_REDUCER_KEY} from 'shared/logic/indicators/reducer';
 import indicator, {INDICATOR_REDUCER_KEY} from 'shared/logic/indicator/reducer';
 import indicatorByCountryData, {INDICATOR_BY_COUNTRY_DATA_REDUCER_KEY} from 'shared/logic/indicatorByCountryData/reducer';
